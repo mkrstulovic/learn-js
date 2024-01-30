7f705886f45f96101348f9fd6a9c3b1ee22e06c5
Marko Krstulovic <mk366d@outlook.com>
2024-01-30 16:56:25 -0500

    Completed classwork on 1/30/2024

--- quiz/scripts/logger.js
+++ quiz/scripts/logger.js
@@ -1,1 +1,15 @@
-// Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg. 
+// Define a JavaScript function called logMsg() that can be used to log an error message for any object that contains the property errMsg.
+function logMsg() {
+    console.log(`Error Message: ${this.errMsg}`);
+}
+
+const ex1 = {
+    errMsg: 'Error in Obj1'
+}
+
+const ex2 = {
+    errMsg: 'Error in Obj2'
+}
+
+logMsg.call(ex1);
+logMsg.call(ex2);
